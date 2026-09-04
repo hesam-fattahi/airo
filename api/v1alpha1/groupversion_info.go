f61f17e11b534706b4997ed1ba0d1e0999e09399
hesam-fattahi <hesam.fattahi.hf@gmail.com>
2026-09-04 13:59:42 +0330

    feat(crd): integrate controller-gen code and manifest generation targets in Makefile

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,3 +1,6 @@
+// Package v1alpha1 contains API Schema definitions for the reliability v1alpha1 API group
+// +kubebuilder:object:generate=true
+// +groupName=reliability.airo.io
 package v1alpha1
 
 import (
